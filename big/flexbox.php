bede36a36698c3dd18796b3d600234f334e7356a
Diego-Cano <64940291+Diego-Cano@users.noreply.github.com>
2021-05-24 15:10:52 -0700

    Add files via upload

---
+++ big/flexbox.php
@@ -0,0 +1,24 @@
+<?php include 'includes/header.php';?>     
+<!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
+
+<p>“The Flexbox Layout (Flexible Box) module (a W3C Candidate Recommendation as of October 2017) aims at providing a more efficient way to lay out, align and distribute space among items in a container, even when their size is unknown and/or dynamic (thus the word “flex”).” - </p>
+<a href="https://css-tricks.com/snippets/css/a-guide-to-flexbox/#flex-direction" target="_blank">css-tricks.com</a>
+<p>Since its release Fle-box has become one of the most popular css options for  web designing. As Coyier says on his website, the ability of altering items within a container is the whole point of using Flexbox. “The main idea behind the flex layout is to give the container the ability to alter its items’ width/height (and order) to best fill the available space (mostly to accommodate all kinds of display devices and screen sizes). A flex container expands items to fill available free space or shrinks them to prevent overflow. Items will be laid out following either the main axis (from main-start to main-end) or the cross axis (from cross-start to cross-end)”- (Coyier)</p>
+<p>Even though the idea of Flexbox sounds easy, it has multiple properties for its container and items.  “Since flexbox is a whole module and not a single property, it involves a lot of things including its whole set of properties. Some of them are meant to be set on the container (parent element, known as flex container) whereas the others are meant to be set on the children (said flex items).” - (Coyier)</p>
+<p>Unfortunately, Flexbox has problems with browser support, making it useless for some older browsers. As Sean Patnode says on quora.com, browser flexbox support is available in most  new browsers. “Flexbox support is available in most new browsers — Firefox, Chrome, Opera, Microsoft Edge and IE 11, newer versions of Android/iOS, etc. However you should be aware that there are still older browsers in use that don't support Flexbox (or do, but support a really old, out-of-date version of it.)” - </p>
+<a href="https://www.quora.com/What-browsers-support-Flexbox" target="_blank">quora.com</a> 
+
+</section> 
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+<h3>Citations</h3>
+<ul>
+  <li>Damme, Tim Van. “A Complete Guide to Flexbox: CSS-Tricks.” CSS, 7 Apr. 2021, css-tricks.com/snippets/css/a-guide-to-flexbox/#flex-direction. </li>
+  <li>Patnode, Sean. “What browsers support Flexbox?”. Quora, 13 Oct. 2019, https://www.quora.com/What-browsers-support-Flexbox\</li>
+</ul>
+
+</aside>
+<!-- END RIGHT COL -->
+<?php include 'includes/footer.php';?>
---
+++ big/galleries.php
@@ -0,0 +1,26 @@
+<?php include 'includes/header.php';?>     
+<!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
+
+<p>“If you are a web designer or developer, then there is a huge possibility that you might have heard about Image Galleries. HTML, CSS  galleries are image galleries constructed with the help of HTML, CSS and JavaScript languages.” -  </p>
+<a href="https://wpshopmart.com/best-html-css-image-gallery/" target="_blank">wpshopmart.com</a>
+<p>“Galleries are a popular way of integrating images into a website, allowing users to see several images clearly at once. There are several diverse methods of generating slide shows such as these, including embedding a simple Lightbox gallery in your website with a source code and implementing the photo gallery via HTML. Another, even simpler method involves using a popular content management system. To do this with a system like TYPO3 or WordPress, users just need to find the right extension and integrate it into the basic framework.” - </p>
+<a href="https://www.ionos.com/digitalguide/websites/web-design/how-to-create-a-photo-gallery-for-your-website/" target="_blank">ionos.com</a> 
+<p>Integrating an image gallery into your website could improve your users’ experience. As ionos.com says on its website, you could be losing users just by having too much content.  “Large blocks of text can often scare visitors off before they even read the content. Graphics and images are therefore hugely important elements of any website, whether they’re used to illustrate a text, to create a personal or emotional bond with the user, or to visualize the details of a product.” -  (ionos)</p>
+<p>“Adding an HTML photo gallery to your site will not only make your page more attractive but will also engage your visitors and increase the time they spend on page. Great pictures provoke comments, especially when they are displayed on a stylish template that enables Cooliris view and lots of transitions and customization options.” - </p>
+<a href="https://www.cincopa.com/media-platform/html-gallery" target="_blank">cincopa.com</a>
+<p>If you want to integrate an image gallery into your webpage/code, you got to choose a method. As mentioned before, you could either use a CMS, library or copy and paste code from  the web. But what is the best method when it comes down to simplicity and reliability?. Well, it depends on your website needs and expectations. As long as you get familiar with the gallery’s layout, you can modify it to suit your project.</p>
+
+</section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+<h3>Citations</h3>
+<ul>
+  <li>“11 Best Free HTML CSS Image Gallery.” Wpshopmart, 2 Sept. 2020, wpshopmart.com/best-html-css-image-gallery/. </li>
+  <li>“Web Design Basics: Creating a Photo Gallery for Your Website.” IONOS Digitalguide, www.ionos.com/digitalguide/websites/web-design/how-to-create-a-photo-gallery-for-your-website/.</li>
+  <li>“HTML Gallery.” Cincopa, www.cincopa.com/media-platform/html-gallery. </li>
+</ul>
+</aside>
+<!-- END RIGHT COL -->
+<?php include 'includes/footer.php';?>
